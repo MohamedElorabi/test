7a5cddd951cd7d1f77812f3b8df1bb5a19d24410
MohamedElorabi <elorabi199@gmail.com>
2022-08-28 00:19:02 +0200

    2 commit

--- database/migrations/2022_08_24_232844_create_products_table.php
+++ database/migrations/2022_08_24_232844_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->char('name');
+            $table->string('name');
             $table->text('description');
             $table->decimal('price', 8, 2);
             $table->softDeletes();
